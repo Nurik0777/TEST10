f1ac26fd7208f259755bc654e886285493184908
Nurik0777 <nurik02kenzhebaev@gmail.com>
2021-06-30 17:40:30 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,42 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var arr = ['ergvvae',234,true,'eufvj',521,'aerbzrbszb',2,true,341,'aergrvfd',53,'hufi','bijbgd',true,'dfgbsga','ergvvae',false,'aergrvfd']
+
+// var brok = [];
+// var k = 0;
+// function greenFild(item) {
+//     var brr = []
+//     let j = 0;
+//     for (let i = 0; i < item.length; i++) {
+//         if (typeof item[i] === 'string') {
+//             brr[j] = item[i]
+//             j++
+//         }else{
+//             continue
+//         }
+//     }
+//     for (let i = 0; i < brr.length; i++) {
+//         if (brr[i].length > 5) {
+//             brok = brr[i]
+//             k++
+//         }else{
+//             continue
+//         }
+//     }
+//     console.log(brok)
+// }
+
+
+
+// greenFild(arr)
